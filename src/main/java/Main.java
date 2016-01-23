0229c84c91b9ac751a5092746e3e1b0c7bd1ada4
dfontana <dmz.sfx@gmail.com>
2016-01-22 21:39:37 -0500

    Restarting window creation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,7 @@ import main.python.PyInterpreter;
 public class Main {
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					HomeFrame frame = new HomeFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+
 	}
 	
 	/** Give the name of the .py file WITHOUT THE EXTENSION and it will launch the python file
