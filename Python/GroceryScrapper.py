0a2fa67b840fc3b641c7b35cdd97c68ae45f644c
Aura Velarde <aura.velarde@gmail.com>
2016-01-23 16:50:08 -0500

    Work in Progress

--- Python/GroceryScrapper.py
+++ Python/GroceryScrapper.py
@@ -5,14 +5,13 @@ import os, sys
 
 from bs4 import BeautifulSoup
 from sys import argv
-from java.lang import String
-from jarray import array
+#from java.lang import String
+#from jarray import array
 
 
 
-#x = raw_input("Enter the website for the recipe from FoodNetwork: ") #Prompts user for website to scrape\
+x = raw_input("Enter the website for the recipe from FoodNetwork: ") #Prompts user for website to scrape\
 #Will only work for Food Network for now
-script, x = argv
 r = requests.get(x)
 code = r.status_code
 
@@ -23,7 +22,7 @@ if code != requests.codes.ok: #Did it have an error?
 ##f = open ("recipe.txt", "w") #Open to write
 recipe = r.text
 
-soup = BeautifulSoup(r.text)
+soup = BeautifulSoup(r.text.encode("utf-8"), "html.parser")
 print soup.prettify()
 #f.write(=recipe.encode('utf-8')).strip()) #Puts the text into a text file with no HTML
 #Have to parse through and find the first Ingredients (with the space)
