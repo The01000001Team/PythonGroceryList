4b7d9cb09448edbf3ecf00278676a6ef071a7980
dfontana <dmz.sfx@gmail.com>
2016-01-22 21:30:00 -0500

    Work on homeframe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ package main.java;
 
 import java.awt.EventQueue;
 
-public class Main {
+import main.python.PyInterpreter;
+
+
+public class main {
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -15,5 +18,13 @@ public class Main {
 				}
 			}
 		});
+	}
+	
+	/** Give the name of the .py file WITHOUT THE EXTENSION and it will launch the python file
+	 * 
+	 * @param fileName Name of the .py file
+	 */
+	public void launchPyFile(String fileName) {
+		PyInterpreter.execPyFile(fileName);
 	}
 }
