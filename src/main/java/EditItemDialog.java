b5dc6d5badad34703c73b93022a5b0415d6b00cc
dfontana <dmz.sfx@gmail.com>
2016-01-22 22:00:50 -0500

    Layout Finished

--- src/main/java/EditItemDialog.java
+++ src/main/java/EditItemDialog.java
@@ -7,48 +7,108 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 public class EditItemDialog extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+	/*
 		try {
 			EditItemDialog dialog = new EditItemDialog();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
+	*/
 
-	/**
-	 * Create the dialog.
-	 */
 	public EditItemDialog() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 307, 158);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
+			JLabel lblQuantity = new JLabel("Quantity:");
+			GridBagConstraints gbc_lblQuantity = new GridBagConstraints();
+			gbc_lblQuantity.anchor = GridBagConstraints.EAST;
+			gbc_lblQuantity.insets = new Insets(0, 0, 5, 5);
+			gbc_lblQuantity.gridx = 0;
+			gbc_lblQuantity.gridy = 0;
+			contentPanel.add(lblQuantity, gbc_lblQuantity);
+		}
+		{
+			textField = new JTextField();
+			GridBagConstraints gbc_textField = new GridBagConstraints();
+			gbc_textField.insets = new Insets(0, 0, 5, 5);
+			gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField.gridx = 1;
+			gbc_textField.gridy = 0;
+			contentPanel.add(textField, gbc_textField);
+			textField.setColumns(10);
+		}
+		{
+			JLabel lblName = new JLabel("Name:");
+			GridBagConstraints gbc_lblName = new GridBagConstraints();
+			gbc_lblName.insets = new Insets(0, 0, 5, 5);
+			gbc_lblName.anchor = GridBagConstraints.EAST;
+			gbc_lblName.gridx = 0;
+			gbc_lblName.gridy = 1;
+			contentPanel.add(lblName, gbc_lblName);
+		}
+		{
+			textField_1 = new JTextField();
+			GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+			gbc_textField_1.gridwidth = 2;
+			gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+			gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField_1.gridx = 1;
+			gbc_textField_1.gridy = 1;
+			contentPanel.add(textField_1, gbc_textField_1);
+			textField_1.setColumns(10);
+		}
+		{
+			JLabel lblType = new JLabel("Type:");
+			GridBagConstraints gbc_lblType = new GridBagConstraints();
+			gbc_lblType.anchor = GridBagConstraints.EAST;
+			gbc_lblType.insets = new Insets(0, 0, 5, 5);
+			gbc_lblType.gridx = 0;
+			gbc_lblType.gridy = 2;
+			contentPanel.add(lblType, gbc_lblType);
+		}
+		{
+			JComboBox comboBox = new JComboBox();
+			GridBagConstraints gbc_comboBox = new GridBagConstraints();
+			gbc_comboBox.gridwidth = 2;
+			gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+			gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_comboBox.gridx = 1;
+			gbc_comboBox.gridy = 2;
+			contentPanel.add(comboBox, gbc_comboBox);
+		}
+		{
+			JButton btnSubmit = new JButton("Submit");
+			btnSubmit.setEnabled(false);
+			GridBagConstraints gbc_btnSubmit = new GridBagConstraints();
+			gbc_btnSubmit.gridwidth = 2;
+			gbc_btnSubmit.insets = new Insets(0, 0, 0, 5);
+			gbc_btnSubmit.anchor = GridBagConstraints.EAST;
+			gbc_btnSubmit.gridx = 1;
+			gbc_btnSubmit.gridy = 3;
+			contentPanel.add(btnSubmit, gbc_btnSubmit);
 		}
 	}
 
