95f8d8ea23503c82e11a3ac521bbe907b24210dd
dfontana <dmz.sfx@gmail.com>
2016-01-22 22:45:22 -0500

    Made UrlListener an anonymous class

--- src/main/java/HomeFrame.java
+++ src/main/java/HomeFrame.java
@@ -3,8 +3,13 @@ package main.java;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
+
+import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JList;
@@ -12,6 +17,8 @@ import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class HomeFrame extends JFrame {
 
@@ -38,14 +45,14 @@ public class HomeFrame extends JFrame {
 		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		JLabel lblInstructions = new JLabel("Instructions:");
 		GridBagConstraints gbc_lblInstructions = new GridBagConstraints();
 		gbc_lblInstructions.insets = new Insets(0, 0, 5, 5);
 		gbc_lblInstructions.gridx = 0;
 		gbc_lblInstructions.gridy = 0;
 		contentPane.add(lblInstructions, gbc_lblInstructions);
-		
+
 		lblYourGroceryList = new JLabel("Your Grocery List:");
 		GridBagConstraints gbc_lblYourGroceryList = new GridBagConstraints();
 		gbc_lblYourGroceryList.gridwidth = 2;
@@ -53,7 +60,7 @@ public class HomeFrame extends JFrame {
 		gbc_lblYourGroceryList.gridx = 1;
 		gbc_lblYourGroceryList.gridy = 0;
 		contentPane.add(lblYourGroceryList, gbc_lblYourGroceryList);
-		
+
 		JLabel lblPasteUrlsInto = new JLabel("Paste URLs into the box below");
 		GridBagConstraints gbc_lblPasteUrlsInto = new GridBagConstraints();
 		gbc_lblPasteUrlsInto.anchor = GridBagConstraints.WEST;
@@ -61,7 +68,7 @@ public class HomeFrame extends JFrame {
 		gbc_lblPasteUrlsInto.gridx = 0;
 		gbc_lblPasteUrlsInto.gridy = 1;
 		contentPane.add(lblPasteUrlsInto, gbc_lblPasteUrlsInto);
-		
+
 		recipeModel = new DefaultListModel();
 		list_recipe = new JList(recipeModel);
 		list_recipe.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
@@ -74,7 +81,7 @@ public class HomeFrame extends JFrame {
 		gbc_list_recipe.gridx = 1;
 		gbc_list_recipe.gridy = 1;
 		contentPane.add(list_recipe, gbc_list_recipe);
-		
+
 		textArea_Url = new JTextArea();
 		textArea_Url.setLineWrap(true);
 		GridBagConstraints gbc_textArea_Url = new GridBagConstraints();
@@ -83,7 +90,7 @@ public class HomeFrame extends JFrame {
 		gbc_textArea_Url.gridx = 0;
 		gbc_textArea_Url.gridy = 2;
 		contentPane.add(textArea_Url, gbc_textArea_Url);
-		
+
 		btnEdit = new JButton("Edit...");
 		btnEdit.setEnabled(false);
 		GridBagConstraints gbc_btnEdit = new GridBagConstraints();
@@ -91,15 +98,62 @@ public class HomeFrame extends JFrame {
 		gbc_btnEdit.gridx = 1;
 		gbc_btnEdit.gridy = 3;
 		contentPane.add(btnEdit, gbc_btnEdit);
-		
+
 		btnDelete = new JButton("Delete...");
 		btnDelete.setEnabled(false);
 		GridBagConstraints gbc_btnDelete = new GridBagConstraints();
 		gbc_btnDelete.gridx = 2;
 		gbc_btnDelete.gridy = 3;
 		contentPane.add(btnDelete, gbc_btnDelete);
-		
-		
+
+		/**************************************************************
+		 * LISTENERS
+		 **************************************************************/
+
+		btnEdit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							EditItemDialog dialog = new EditItemDialog();
+							dialog.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int index = list_recipe.getSelectedIndex();
+				recipeModel.remove(index);
+
+				int size = recipeModel.getSize();
+
+				if (size == 0){ 
+					btnDelete.setEnabled(false);
+				}
+				else{
+					if (index == size) { index--;}
+				}
+			}
+		});
+
+
+		class UrlListener implements DocumentListener{
+			public void changedUpdate(DocumentEvent e){check();}
+			public void removeUpdate(DocumentEvent e) {check();}
+			public void insertUpdate(DocumentEvent e) {check();}
+			private void check(){
+				/*if(instructorNameValue.getText().trim().equals("") || courseNameValue.getText().trim().equals("")){
+					btnSubmit.setEnabled(false);
+				}else{
+					btnSubmit.setEnabled(true);
+				}*/
+			}
+		}
 		UrlListener urlListener = new UrlListener();
 		textArea_Url.getDocument().addDocumentListener(urlListener);
 	}
