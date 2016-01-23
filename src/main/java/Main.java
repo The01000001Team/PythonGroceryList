57d48c55e70d3eb6e78d1086c9c1767047c44b41
dfontana <dmz.sfx@gmail.com>
2016-01-22 21:10:00 -0500

    Tied frame to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 package main.java;
 
+import java.awt.EventQueue;
+
 public class Main {
-	//java stuff
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomeFrame frame = new HomeFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
