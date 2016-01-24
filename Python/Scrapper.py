7ed7ffe7d1a15957b528cb952c427edf932cbb1e
Aura Velarde <aura.velarde@gmail.com>
2016-01-23 22:58:34 -0500

    Made changes to Scrapper to work with beautiful soup, functioning

--- Python/Scrapper.py
+++ Python/Scrapper.py
@@ -2,27 +2,25 @@ import requests
 import unicodedata
 import os, sys
 
-
+from bs4 import BeautifulSoup
 from sys import argv
 from java.lang import String
 from jarray import array
-
-
 
 script, x = argv
 try:
     r = requests.get(x)
     code = r.status_code
 except:
-    jythonArray = None
+	jythonArray = None
     sys.exit()
 
-soup = BeautifulSoup(r.text.encde("utf-8", "ignore"), 'html.parser') #Parse through HTML 
+soup = BeautifulSoup(r.text.encode("utf-8", "ignore"), 'html.parser') #Parse through HTML 
 list = [] #List for the recipe ingredients 
 
-for ingredient in soup.find_all('li', itemprop_='ingredients'):
+for ingredient in soup.find_all('li', itemprop="ingredients"): #, itemprop_='ingredients'
 	list.append(unicodedata.normalize('NFKD',((ingredient.text).strip(' \t\n\r'))).encode('ascii','ignore'))
 
-
+	
 jythonArray = array(list, String)
 
