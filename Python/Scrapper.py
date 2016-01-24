ff3262121216b895fcbf3c8ad63603f2010dd6e1
Aura Velarde <aura.velarde@gmail.com>
2016-01-23 22:02:28 -0500

    Added Beautiful Soup to Original Scrapper

--- Python/Scrapper.py
+++ Python/Scrapper.py
@@ -1,5 +1,4 @@
 import requests
-import re
 import unicodedata
 import os, sys
 
@@ -8,14 +7,8 @@ from sys import argv
 from java.lang import String
 from jarray import array
 
-TAG_RE = re.compile(r'<[^>]+>')
-
-def remove_tags(text): #Remove HTML Tags (I know.. the post.. but very specific use!)
-    return TAG_RE.sub('', text)
 
 
-#x = raw_input("Enter the website for the recipe from FoodNetwork: ") #Prompts user for website to scrape\
-#Will only work for Food Network for now
 script, x = argv
 try:
     r = requests.get(x)
@@ -24,28 +17,12 @@ except:
     jythonArray = None
     sys.exit()
 
+soup = BeautifulSoup(r.text.encde("utf-8", "ignore"), 'html.parser') #Parse through HTML 
+list = [] #List for the recipe ingredients 
 
-# if code != requests.codes.ok: #Did it have an error?
-# 	System.out.println("Sorry, there was an error and cannot proceed")
-# 	exit()
-
-f = open ("recipe.txt", "w") #Open to write
-recipe = r.text
-f.write(remove_tags(recipe.encode('utf-8')).strip()) #Puts the text into a text file with no HTML
-#Have to parse through and find the first Ingredients (with the space)
-f.close()
-
-list = []
-
-f = open ("recipe.txt", "r")
-for line in f:
-	if "Ingredients" in line and not "Ingredients\"" in line: #Does the list of ingredients start?
-		while not "Directions" in line: #Print until directions
-			if not line.isspace(): #Avoid empty lines
-				list.append(unicodedata.normalize('NFKD',(line.strip(' \t\n\r'))).encode('ascii','ignore'))
-			line = next(f)	#Proceed to the next line in the file
+for ingredient in soup.find_all('li', itemprop_='ingredients'):
+	list.append(unicodedata.normalize('NFKD',((ingredient.text).strip(' \t\n\r'))).encode('ascii','ignore'))
 
 
 jythonArray = array(list, String)
-f.close() #Close the text file
-os.remove(f.name) #removes the file from memory
+
